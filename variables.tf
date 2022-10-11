75a721382c15f91666d105037526d89052596be2
tomarv2 <varuntomar2019@gmail.com>
2022-10-11 19:13:00 +0000

    Sync from management repo

--- variables.tf
+++ variables.tf
@@ -1,7 +1,21 @@
 variable "teamid" {
   description = "Name of the team/group e.g. devops, dataengineering. Should not be changed after running 'tf apply'"
+  type        = string
 }
 
 variable "prjid" {
   description = "Name of the project/stack e.g: mystack, nifieks, demoaci. Should not be changed after running 'tf apply'"
+  type        = string
+}
+
+variable "activity_logs_config" {
+  description = "Azure monitor activity log alert configuration"
+  type        = map(any)
+}
+
+
+variable "extra_tags" {
+  description = "Additional tags to associate"
+  type        = map(string)
+  default     = {}
 }
